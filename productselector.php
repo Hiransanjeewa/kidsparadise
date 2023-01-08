3b32cc4f8c0cd768e4fb13780887565b70fa4a9c
Hiransanjeewa <94454389+Hiransanjeewa@users.noreply.github.com>
2023-01-08 01:17:21 -0800

    product selector

---
+++ productselector.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="product.php" method="get">
+            <select name="productID">
+                <option value="00001">Jeep</option>
+                <option value="00002">Helecopter</option>
+            </select>
+            <input type="submit" name="submit">
+        </form>
+        
+    </body>
+</html>
